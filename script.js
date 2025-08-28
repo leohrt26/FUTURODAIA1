e87078a07558c8152ecf33347a719e9ee42b475c
leohrt26 <leonardo.hartman.souza@escola.pr.gov.br>
2025-08-28 11:37:56 -0300

    l

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const caixaPrincipal - document. querySelector("-caixa-principal"); const caixaPerguntas - document. querySelector(".caixa-perguntas*); const caixaAlternativas - document.querySelector(".caixa-alternativas*);
+const caixaResultado - document. querySelector(*-caixa-resultado");
+const textoResultado = document. querySelector(*.texto-resultado");
+
+const
+perguntas = [
+enunciado: "você acha que a IA vai mudar o mundo ?", alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+},
+enunciado: "Pergunta 2", alternativas: [
+"Alternativa 1",
+"Alternativa 2"
+enunciado: "Pergunta 3", alternativas: [
+"Alternativa 1",
+"Alternativa 2"
