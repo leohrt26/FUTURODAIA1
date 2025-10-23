57f59c360d875ca4decee086eb0ec5688478ba81
leohrt26 <leonardo.hartman.souza@escola.pr.gov.br>
2025-10-23 11:08:32 -0300

    le

--- script.js
+++ script.js
@@ -20,3 +20,11 @@ enunciado: "Oque significa IA", alternativas: [
 enunciado: "Qual tecnologia permite que a IA aprenda com dados", alternativas: [
 "Machine Learning",
 "WI-FI"
+
+enunciado:"Qual dessas e uma aplicação comum de IA", alternativas: [ 
+    "previsão do tempo"
+    "impressão de documentos"
+    
+    enunciado:"Qual e uma linguagem de programação usada em IA", alternativas: [
+    "PYTHON
+    "excel"
